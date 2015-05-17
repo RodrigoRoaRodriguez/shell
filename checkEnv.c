bb1e8c0de78e98878051fae0bfd1d7a0e1a8ce5c
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2015-05-17 21:27:41 +0200

    Started implementing printenv|sort|less

--- checkEnv.c
+++ checkEnv.c
@@ -9,16 +9,15 @@
 
 int main()
 {
-  pid_t pidPrintenv, pidFilter;
-  int pipa[2], status;
+  int pipa[2];
   int pid;
 
   pipe(pipa);
 
   printf("pipa0 %d pip1 %d\n", pipa[READ], pipa[WRITE]);
 
+  // Divide this process into two.
   pid = fork();
-
 
   if( pid == 0) //printenv process
   {
@@ -27,16 +26,29 @@ int main()
     close(pipa[READ]);
     execlp("printenv", "printenv", NULL);
     exit(0); // Close pipe so that results are printed
-  }
-  if(pid != 0)//filter process
-  {
-    close(pipa[WRITE]);
-    dup2(pipa[READ], READ);
-    close(pipa[READ]);
-    execlp("sort","sort",NULL);
-    exit(0);
+
+  }else{ //filter process
+
+    // Divide this process into two.
+    pid = fork();
+
+    if( pid == 0) //printenv process
+    {
+
+      dup2(pipa[WRITE], WRITE);
+      close(pipa[WRITE]);
+      close(pipa[READ]);
+      execlp("sort", "sort", NULL);
+      exit(0); // Close pipe so that results are printed
+
+    }else{ //filter process
+
+      close(pipa[WRITE]);
+      dup2(pipa[READ], READ);
+      close(pipa[READ]);
+      execlp("less","less",NULL);
+      exit(0);
+    }
 
   }
-  wait(&status);
-  wait(&status);
 }
